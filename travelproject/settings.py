88f3ec36bb3883ed0cfd2cc885cb3d3ec5410207
Farha Islam <farha.islam@students.codeinstitute.net>
2024-07-31 10:27:54 +0000

    Update requirements and settings

--- travelproject/settings.py
+++ travelproject/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-2njf4c7qf!1($hvonyg92%6b^v76cd@n%#!fk^26*ot9nib&vi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-farhatamann-travelwithf-zy9tex332bb.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-farhatamann-travelwithf-zy9tex332bb.ws.codeinstitute-ide.net'
+,'.herokuapp.com']
 
 
 # Application definition
