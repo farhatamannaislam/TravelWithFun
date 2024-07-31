c6d64ce286ee5227a9d421116b52fe05e5d9b771
Farha Islam <farha.islam@students.codeinstitute.net>
2024-07-31 12:42:39 +0000

    Add secret key

--- travelproject/settings.py
+++ travelproject/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2njf4c7qf!1($hvonyg92%6b^v76cd@n%#!fk^26*ot9nib&vi'
-
+#SECRET_KEY = 'django-insecure-2njf4c7qf!1($hvonyg92%6b^v76cd@n%#!fk^26*ot9nib&vi'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
