ae21ccb510f146d06d14f040518195036689c054
Farha Islam <farha.islam@students.codeinstitute.net>
2024-08-01 09:33:42 +0000

    Add generic view

--- travelproject/urls.py
+++ travelproject/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('travelblog.urls'))
+    path('',include('travelblog.urls'), name = "travelblog-urls")
 ]
