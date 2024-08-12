aad0e42f6d6a2ad2f8ca9f876292769d3e911aef
Farha Islam <farha.islam@students.codeinstitute.net>
2024-08-12 07:25:11 +0000

    Test: Cloudinary functionality

--- siteuser/models.py
+++ siteuser/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import FileExtensionValidator
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
 
 class ProfileModel(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.png', upload_to='profile', validators = [FileExtensionValidator(['png', 'jpg'])])
+    image = CloudinaryField('image', default='default.png', validators=[FileExtensionValidator(['png', 'jpg'])])
     def __str__(self):
         return self.user.username
