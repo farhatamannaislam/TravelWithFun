f4888b433ca12dbbabc25cd5a76f0ae34493714a
Farha Islam <farha.islam@students.codeinstitute.net>
2024-08-15 18:06:42 +0000

    Update: Cloudinary validation

--- siteuser/models.py
+++ siteuser/models.py
@@ -2,15 +2,29 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import FileExtensionValidator
 from cloudinary.models import CloudinaryField
+from django.core.exceptions import ValidationError
+from cloudinary import CloudinaryResource
 
 # Create your models here.
+def validate_cloudinary_file_extension(value):
+    """
+    Validate cloudinary files 
+    """ 
+    if isinstance(value, CloudinaryResource):
+        extension = value.public_id.split('.')[-1].lower()
+    else:
+        extension = value.name.split('.')[-1].lower()
+
+    valid_extensions = ['png', 'jpg']
+    if extension not in valid_extensions:
+        raise ValidationError(f'Unsupported file extension: {extension}. Allowed extensions are: {", ".join(valid_extensions)}')
 
 
 class ProfileModel(models.Model):
     """
-    Represents User Profile with associated image
-     """    
+    Display user Profile
+    """    
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = CloudinaryField('image', default='default.png', validators=[FileExtensionValidator(['png', 'jpg'])])
+    image = CloudinaryField('image', default='default.png', validators=[validate_cloudinary_file_extension])
     def __str__(self):
         return self.user.username
