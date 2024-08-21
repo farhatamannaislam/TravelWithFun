32037085fe078b7bd2aa67d5149230f607144b6f
Farha Islam <farha.islam@students.codeinstitute.net>
2024-08-21 12:42:43 +0000

    Remove: Unnecessary comments

--- travelproject/settings.py
+++ travelproject/settings.py
@@ -25,7 +25,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-2njf4c7qf!1($hvonyg92%6b^v76cd@n%#!fk^26*ot9nib&vi'
+
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
