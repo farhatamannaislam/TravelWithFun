ed254bece069ea2dc1f3d826b46bbc19dce18057
farha <farha.islam@students.codeinstitute.net>
2025-06-03 15:15:46 +0200

    Update: staticfiles

--- travelproject/settings.py
+++ travelproject/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = False
 
 ALLOWED_HOSTS = ['8000-farhatamann-travelwithf-zy9tex332bb.ws.codeinstitute-ide.net'
-,'.herokuapp.com']
+,'.herokuapp.com','*']
 
 
 # Application definition
